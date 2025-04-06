2fe6342b178ce17787b1045d44e2a508bda1b704
LimJungWoo <presidentljw@naver.com>
2025-04-06 13:07:17 +0900

    fix(entity): Remove manual default from @CreateDateColumn

--- src/common/entities/abstract.entity.ts
+++ src/common/entities/abstract.entity.ts
@@ -11,7 +11,7 @@ export abstract class AbstractEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @CreateDateColumn({ name: 'created_at', default: () => 'CURRENT_TIMESTAMP' })
+  @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
   @UpdateDateColumn({ name: 'updated_at' })
